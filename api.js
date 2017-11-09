a00ded0d9a62b1025809fc59671f8b91bb74a730
kelvin <kervin_0708@hotmail.com>
2017-11-09 05:11:09 -0600

    feat:Crud Menu

--- api.js
+++ api.js
@@ -711,6 +711,210 @@ module.exports = function (wagner) {
   }));
 
 
+  //Menu
+
+
+  api.get('/menu', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+
+        Menu.find().populate(
+          [
+            {
+              path: "nIdTipoMenu", model: "TipoM", select: {
+                'cDescripcion': 1, '_id': 0
+              },
+            },
+            {
+              path: "oComida.nIdProducto", model: "Producto", select: {
+                'cDescripcion': 1, '_id': 0
+              }
+            }
+          ]).exec(handleMany.bind(null, 'Menu', res));
+      }
+      catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+
+  api.get('/menu/id/:id', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        Menu.findOne({ _id: req.params.id }, function (error, menu) {
+          if (error) {
+            return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+          }
+          if (!menu) {
+            return res.status(status.NOT_FOUND).json({ Codigo: status.NOT_FOUND, Mensaje: 'Menu inexistente', Detalle: '' });
+          }
+          //return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Operación exitosa', Menu: menu });
+        }).populate(
+          [
+            {
+              path: "nIdTipoMenu", model: "TipoM", select: {
+                'cDescripcion': 1, '_id': 0
+              },
+            },
+            {
+              path: "oComida.nIdProducto", model: "Producto", select: {
+                'cDescripcion': 1, '_id': 0
+              }
+            }
+          ]).exec(handleOne.bind(null,"Menu",res));
+          
+      }
+      catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+  api.get('/menu/food/:id', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        Menu.findOne({ _id: req.params.id }, function (error, menu) {
+          if (error) {
+            return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+          }
+          if (!clasificacion) {
+            return res.status(status.NOT_FOUND).json({ Codigo: status.NOT_FOUND, Mensaje: 'Menu inexistente', Detalle: '' });
+          }
+          return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Operación exitosa', Comida: menu.oComida });
+        });
+      }
+      catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+  api.post('/menu/add', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        var datos = req.body.Menu;
+      }
+      catch (e) {
+        return res.status(status.BAD_REQUEST).json({ Codigo: status.BAD_REQUEST, Mensaje: 'Clasificacion no especificada', Detalle: e.message });
+
+      }
+      try {
+        Menu.create({
+          cNombre: datos.Nombre,
+          nIdTipoMenu: datos.IdTipoMenu,
+          cDescripcion: datos.Descripcion,
+          oComida: datos.Comida
+        }, function (error, menu) {
+          if (error) {
+            return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+          }
+
+          if (!menu) {
+            return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "El menu no fue registrado", Detalle: '' });
+          }
+          return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Registro exitoso', Detalle: '' });
+        });
+      } catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+
+  api.put('/menu/update', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        var datos = req.body.Menu;
+      }
+      catch (e) {
+        return res.status(status.BAD_REQUEST).json({ Codigo: status.BAD_REQUEST, Mensaje: 'Clasificacion no especificada', Detalle: e.message });
+      }
+      try {
+        Menu.findOneAndUpdate({ "_id": datos.IdMenu },
+          {
+            $set:
+            {
+              "cNombre": datos.Nombre,
+              "nIdTipoMenu": datos.Clasificacion,
+              "cDescripcion": datos.Descripcion
+            }
+          }, function (error, menu) {
+            if (error) {
+              return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+            }
+            if (!menu) {
+              return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "El menú no existe", Detalle: '' });
+            }
+            return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Registro actualizado', Detalle: '' });
+          });
+      } catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+
+
+  //Asignar producto
+  api.put('/menu/food/add', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        var datos = req.body.Porcion;
+      }
+      catch (e) {
+        return res.status(status.BAD_REQUEST).json({ Codigo: status.BAD_REQUEST, Mensaje: 'Producto no especificado', Detalle: e.message });
+      }
+      try {
+        Menu.findOne({ _id: datos.IdMenu }, function (error, menu) {
+          if (error) {
+            return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+          }
+          if (!menu) {
+            return res.status(status.NOT_FOUND).json({ Codigo: status.NOT_FOUND, Mensaje: 'Menu inexistente', Detalle: '' });
+          }
+
+          menu.oComida.push({ nIdProducto: datos.Producto.IdProducto, nCantidad: datos.Producto.Cantidad, nCalorias: datos.Producto.Calorias });
+          menu.save(function (error) {
+            if (error) {
+              return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+            }
+            return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Registro exitoso', Detalle: '' });
+          });
+        });
+      }
+      catch (e) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: e.message });
+      }
+    }
+  }));
+
+  //Eliminar producto
+  api.delete('/menu/food/delete', wagner.invoke(function (Menu) {
+    return function (req, res) {
+      try {
+        var datos = req.body.Porcion;
+      }
+      catch (e) {
+        return res.status(status.BAD_REQUEST).json({ Codigo: status.BAD_REQUEST, Mensaje: 'Producto no especificado', Detalle: e.message });
+      }
+
+      Menu.findOne({ _id: datos.IdMenu }, function (error, menu) {
+        var indice = menu.oComida.findIndex(x => x.nIdProducto == datos.IdProducto);
+        if (indice != -1) {
+          menu.oComida.splice(indice, 1);
+          menu.save(function (error) {
+            if (error) {
+              return res.status(status.INTERNAL_SERVER_ERROR).json({ Codigo: status.INTERNAL_SERVER_ERROR, Mensaje: "Ha ocurrido un problema", Detalle: error.toString() });
+            }
+          });
+        }
+      });
+      return res.status(status.OK).json({ Codigo: status.OK, Mensaje: 'Registro actualizado', Detalle: '' });
+    }
+  }));
+
+
   return api;
 }
 
